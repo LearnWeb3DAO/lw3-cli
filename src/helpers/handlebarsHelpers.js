6a187b65cd4c0024f089fb50290ebf75f10d1483
ehsan-qaderi <qaderi.coding@gmail.com>
2022-04-13 14:33:32 +0430

    trim and pascalcase handlebarjs helper methods added

--- src/helpers/handlebarsHelpers.js
+++ src/helpers/handlebarsHelpers.js
@@ -3,12 +3,39 @@
  * @param {Handlebars} hbs
  */
 
-export const registerHelpers = (hbs) => {
-  hbs.registerHelper("upperCase", (value) => {
-    return value.toUpperCase();
-  });
+import { toPascalCase } from "./helpers";
 
-  hbs.registerHelper("lowerCase", (value) => {
-    return value.toLowerCase();
-  });
+export const registerHelpers = (hbs) => {
+  try {
+    hbs.registerHelper("upperCase", (value) => {
+      return value.toUpperCase();
+    });
+  } catch (error) {
+    console.error(error)
+  }
+ 
+  try {
+    hbs.registerHelper("lowerCase", (value) => {
+      return value.toLowerCase();
+    });
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    hbs.registerHelper("pascalCase", (value) => {
+      return toPascalCase(value)
+    });
+  } catch (error) {
+    console.error(error)
+  }
+  
+  try {
+    hbs.registerHelper("trimInput", (value) => {
+      return value.trim()
+    });
+  } catch (error) {
+    console.error(error)
+  }
+ 
 };
