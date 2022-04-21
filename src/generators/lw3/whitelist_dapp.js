0cb1bfd17857f326a6eff0843a3081e646724952
Jordan Baron <jordannbaron@gmail.com>
2022-04-21 13:58:35 -0400

    Whitelist dApp generator

--- src/generators/lw3/whitelist_dapp.js
+++ src/generators/lw3/whitelist_dapp.js
@@ -1,13 +1,33 @@
 import { runInstructions } from "../../helpers";
 import { installHardhat } from "../";
 import { createNextApp } from "../";
+import generateHardhatFiles from "../backend/generateHardhatFiles";
+import inquirer from "inquirer";
 
 export default {
   generate: async function () {
-    await installHardhat();
-    const next_folder = await createNextApp();
+    const { network } = await inquirer.prompt([
+      {
+        name: "network",
+        type: "list",
+        message: "Network: ",
+        choices: ["Rinkeby", "Ropsten"],
+        default: "Rinkeby",
+      },
+    ]);
+
+    const hardhatFolder = await installHardhat(false, true, false);
+    await generateHardhatFiles(
+      false,
+      hardhatFolder,
+      network,
+      "Whitelist",
+      false
+    );
+    const nextFolder = await createNextApp(false);
     const instructions = [
-      `echo "export const abi = YOUR_ABI;\nexport const WHITELIST_CONTRACT_ADDRESS = 'YOUR_WHITELIST_CONTRACT_ADDRESS'" >> ${next_folder}/constants/index.js`,
+      `mkdir ${nextFolder}/constants`,
+      `echo "export const abi = YOUR_ABI;\nexport const WHITELIST_CONTRACT_ADDRESS = 'YOUR_WHITELIST_CONTRACT_ADDRESS'" >> ${nextFolder}/constants/index.js`,
     ];
     await runInstructions(instructions);
   },
