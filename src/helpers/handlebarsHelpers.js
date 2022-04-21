511711a07d5a37db5fabc91b21adc6d8af5861f0
Jordan Baron <jordannbaron@gmail.com>
2022-04-21 13:58:22 -0400

    Check if value is of type string before trying to do anything with it

--- src/helpers/handlebarsHelpers.js
+++ src/helpers/handlebarsHelpers.js
@@ -8,58 +8,72 @@ import { toPascalCase } from "./helpers";
 export const registerHelpers = (hbs) => {
   try {
     hbs.registerHelper("upperCase", (value) => {
-      return value.toUpperCase();
+      if (typeof value === "string") {
+        return value.toUpperCase();
+      }
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
- 
+
   try {
     hbs.registerHelper("lowerCase", (value) => {
-      return value.toLowerCase();
+      if (typeof value === "string") {
+        return value.toLowerCase();
+      }
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 
   try {
     hbs.registerHelper("camelCase", (value) => {
-      return value.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+      if (typeof value === "string") {
+        return value
+          .toLowerCase()
+          .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+      }
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 
   try {
     hbs.registerHelper("pascalCase", (value) => {
-      return toPascalCase(value)
+      if (typeof value === "string") {
+        return toPascalCase(value);
+      }
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
+
   try {
     hbs.registerHelper("trimInput", (value) => {
-      return value.trim()
+      if (typeof value === "string") {
+        return value.trim();
+      }
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 
   try {
+    hbs.registerHelper(
+      "when",
+      function (operand_1, operator, operand_2, options) {
+        var operators = {
+            eq: function (l, r) {
+              return l == r;
+            },
+          },
+          result = operators[operator](operand_1, operand_2);
 
-    hbs.registerHelper("when",function(operand_1, operator, operand_2, options) {
-      var operators = {
-       'eq': function(l,r) { return l == r; },
+        if (result) return options.fn(this);
+        else return options.inverse(this);
       }
-      , result = operators[operator](operand_1,operand_2);
-    
-      if (result) return options.fn(this);
-      else  return options.inverse(this);
-    });
-    
+    );
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
- 
 };
