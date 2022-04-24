6da9698c8c4f36a5a0d38609ce1326548b223fe1
ehsan-qaderi <qaderi.coding@gmail.com>
2022-04-24 23:16:16 +0430

    add template detection

--- src/generators/backend/getHardhatDataInputs.js
+++ src/generators/backend/getHardhatDataInputs.js
@@ -2,79 +2,103 @@ import inquirer from "inquirer";
 import Handlebars from "handlebars";
 import { registerHelpers } from "../../helpers";
 import generateHardhatFiles from "./generateHardhatFiles";
-
+import { getTemplateType } from "../../helpers"
+import { existsSync } from "fs"
+import chalk from "chalk"
 registerHelpers(Handlebars);
 
 /**
  * Prompts the user for data including the name of the contract to be generated,
  * the network to deploy the contract to, and, if the user wants, what OpenZeppelin standards to override
  * It will then call generateHardhatFiles with the data
+ * @param {boolean} verbose - Whether or not to print success messages/instructions
+
  */
 
 const getHardhatDataInputs = async () => {
-  const { contract } = await inquirer.prompt([
-    {
-      name: "contract",
-      type: "input",
-      message: "Enter contract name: ",
-    },
-  ]);
+  if (existsSync("contracts")) {
+    let extension = "js"
 
-  const { network } = await inquirer.prompt([
-    {
-      name: "network",
-      type: "list",
-      message: "Network: ",
-      choices: ["Rinkeby", "Ropsten"],
-      default: "Rinkeby",
-    },
-  ]);
+    //Determine wether the user uses typescript/javascript in backend
+   
+    const templateType = await getTemplateType("backend")
 
-  const { openzeppelinConfirmation } = await inquirer.prompt([
-    {
-      name: "openzeppelinConfirmation",
-      type: "confirm",
-      message: "Do you want to implement openzeppelin token standards?",
-      default: false,
-    },
-  ]);
-
-  if (openzeppelinConfirmation) {
-    const { tokenType } = await inquirer.prompt([
+    if (templateType == "typescript") {
+      extension = "ts"
+    }
+    const { contract } = await inquirer.prompt([
       {
-        name: "tokenType",
+        name: "contract",
+        type: "input",
+        message: "Enter contract name: ",
+      },
+    ]);
+
+    const { network } = await inquirer.prompt([
+      {
+        name: "network",
         type: "list",
-        message: "Select token type:",
-        choices: ["ERC1155", "ERC20", "ERC721"],
+        message: "Network: ",
+        choices: ["Rinkeby", "Ropsten"],
+        default: "Rinkeby",
       },
     ]);
 
-    const { tokenName } = await inquirer.prompt([
+    const { openzeppelinConfirmation } = await inquirer.prompt([
       {
-        name: "tokenName",
-        type: "input",
-        message: "Enter token name:",
+        name: "openzeppelinConfirmation",
+        type: "confirm",
+        message: "Do you want to implement openzeppelin token standards?",
+        default: false,
       },
     ]);
 
-    const { tokenSymbol } = await inquirer.prompt([
-      {
-        name: "tokenSymbol",
-        type: "input",
-        message: "Enter token symbol:",
-      },
-    ]);
+    if (openzeppelinConfirmation) {
+      const { tokenType } = await inquirer.prompt([
+        {
+          name: "tokenType",
+          type: "list",
+          message: "Select token type:",
+          choices: ["ERC1155", "ERC20", "ERC721"],
+        },
+      ]);
 
-    generateHardhatFiles(
-      network,
-      contract,
-      openzeppelinConfirmation,
-      tokenType,
-      tokenName,
-      tokenSymbol
-    );
+      const { tokenName } = await inquirer.prompt([
+        {
+          name: "tokenName",
+          type: "input",
+          message: "Enter token name:",
+        },
+      ]);
+
+      const { tokenSymbol } = await inquirer.prompt([
+        {
+          name: "tokenSymbol",
+          type: "input",
+          message: "Enter token symbol:",
+        },
+      ]);
+
+      generateHardhatFiles(
+        true,
+        extension,
+        network,
+        contract,
+        openzeppelinConfirmation,
+        tokenType,
+        tokenName,
+        tokenSymbol
+      );
+
+    } else {
+      generateHardhatFiles(true, extension, network, contract, openzeppelinConfirmation);
+    }
   } else {
-    generateHardhatFiles(network, contract, openzeppelinConfirmation);
+    console.log(
+      chalk.red(
+        "Hardhat directory is not found!\nPlease, switch to a directory where Hardhat is installed!"
+      )
+    );
   }
 };
 
