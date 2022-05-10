11faf32711499aa138e9e6f675c337ffd274750a
ehsan-qaderi <qaderi.coding@gmail.com>
2022-05-10 21:37:20 +0430

    add testnet function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,15 +1,21 @@
-/*
-Barrel file
-Make every function accessible by just importing helpers
-e.g. import {registerHelpers} from "../helpers"
-instead of import {registerHelpers} from "../helpers/handlebarsHelpers"
-*/
-export { registerHelpers } from "./handlebarsHelpers";
-export { runInstructions } from "./runInstructions";
+/**
+ * /*
+ * Barrel file
+ * Make every function accessible by just importing helpers
+ * e.g. import {registerHelpers} from "../helpers"
+ * instead of import {registerHelpers} from "../helpers/handlebarsHelpers"
+ *
+ * @format
+ */
+
+export { registerHelpers } from './handlebarsHelpers';
+export { runInstructions } from './runInstructions';
 export {
-getProjectGenerator,
-getProjectsFromTrack,
-getWelcomeMessage,
-getCLIInstructions,
-getTemplateType
-} from "./helpers";
+  getProjectGenerator,
+  getProjectsFromTrack,
+  getWelcomeMessage,
+  getCLIInstructions,
+  getTemplateType,
+  trimInput,
+  getTestnetNetworks,
+} from './helpers';
