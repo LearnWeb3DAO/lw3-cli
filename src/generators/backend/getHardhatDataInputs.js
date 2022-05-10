adf7060be9344506d3796100a9ac6dd25644322e
ehsan-qaderi <qaderi.coding@gmail.com>
2022-05-10 21:35:13 +0430

    change imports

--- src/generators/backend/getHardhatDataInputs.js
+++ src/generators/backend/getHardhatDataInputs.js
@@ -1,11 +1,13 @@
-import inquirer from "inquirer";
-import Handlebars from "handlebars";
-import { registerHelpers } from "../../helpers";
-import generateHardhatFiles from "./generateHardhatFiles";
-import { getTemplateType } from "../../helpers"
-import { existsSync } from "fs"
-import chalk from "chalk"
-import { trimInput } from "../../helpers/helpers";
+/** @format */
+
+import inquirer from 'inquirer';
+import Handlebars from 'handlebars';
+import { registerHelpers } from '../../helpers';
+import generateHardhatFiles from './generateHardhatFiles';
+import { getTemplateType } from '../../helpers';
+import { existsSync } from 'fs';
+import chalk from 'chalk';
+import { trimInput, getTestnetNetworks } from '../../helpers';
 registerHelpers(Handlebars);
 
 /**
@@ -17,46 +19,46 @@ registerHelpers(Handlebars);
  */
 
 const getHardhatDataInputs = async () => {
-  if (existsSync("contracts")) {
-    let extension = "js"
+  if (existsSync('contracts')) {
+    let extension = 'js';
 
     //Determine wether the user uses typescript/javascript in backend
 
-    const templateType = await getTemplateType("backend")
+    const templateType = await getTemplateType('backend');
 
-    if (templateType === "typescript") {
-      extension = "ts"
+    if (templateType === 'typescript') {
+      extension = 'ts';
     }
     let { contract } = await inquirer.prompt([
       {
-        name: "contract",
-        type: "input",
-        message: "Enter contract name: ",
+        name: 'contract',
+        type: 'input',
+        message: 'Enter contract name: ',
         validate: async (input) => {
           if (input.length == 0) {
-             return 'contract name can not be empty';
+            return 'contract name can not be empty';
           }
           return true;
-       }
+        },
       },
     ]);
-    contract = trimInput(contract)
+    contract = trimInput(contract);
 
     const { network } = await inquirer.prompt([
       {
-        name: "network",
-        type: "list",
-        message: "Network: ",
-        choices: ["Rinkeby", "Ropsten"],
-        default: "Rinkeby",
+        name: 'network',
+        type: 'list',
+        message: 'Network: ',
+        choices: getTestnetNetworks(),
+        default: 'Goerli',
       },
     ]);
 
     const { openzeppelinConfirmation } = await inquirer.prompt([
       {
-        name: "openzeppelinConfirmation",
-        type: "confirm",
-        message: "Do you want to implement openzeppelin token standards?",
+        name: 'openzeppelinConfirmation',
+        type: 'confirm',
+        message: 'Do you want to implement openzeppelin token standards?',
         default: false,
       },
     ]);
@@ -64,38 +66,38 @@ const getHardhatDataInputs = async () => {
     if (openzeppelinConfirmation) {
       const { tokenType } = await inquirer.prompt([
         {
-          name: "tokenType",
-          type: "list",
-          message: "Select token type:",
-          choices: ["ERC1155", "ERC20", "ERC721"],
+          name: 'tokenType',
+          type: 'list',
+          message: 'Select token type:',
+          choices: ['ERC1155', 'ERC20', 'ERC721'],
         },
       ]);
 
       const { tokenName } = await inquirer.prompt([
         {
-          name: "tokenName",
-          type: "input",
-          message: "Enter token name:",
+          name: 'tokenName',
+          type: 'input',
+          message: 'Enter token name:',
           validate: async (input) => {
             if (input.length == 0) {
-               return 'token name can not be empty';
+              return 'token name can not be empty';
             }
             return true;
-         }
+          },
         },
       ]);
 
       const { tokenSymbol } = await inquirer.prompt([
         {
-          name: "tokenSymbol",
-          type: "input",
-          message: "Enter token symbol:",
+          name: 'tokenSymbol',
+          type: 'input',
+          message: 'Enter token symbol:',
           validate: async (input) => {
             if (input.length == 0) {
-               return 'token symbol can not be empty';
+              return 'token symbol can not be empty';
             }
             return true;
-         }
+          },
         },
       ]);
 
@@ -109,14 +111,19 @@ const getHardhatDataInputs = async () => {
         tokenName,
         tokenSymbol
       );
-
     } else {
-      generateHardhatFiles(true, extension, network, contract, openzeppelinConfirmation);
+      generateHardhatFiles(
+        true,
+        extension,
+        network,
+        contract,
+        openzeppelinConfirmation
+      );
     }
   } else {
     console.log(
       chalk.red(
-        "Hardhat directory is not found!\nPlease, switch to a directory where Hardhat is installed!"
+        'Hardhat directory is not found!\nPlease, switch to a directory where Hardhat is installed!'
       )
     );
   }
